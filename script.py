fa034fad102552893cb52cc8c0781d8dbd22cf53
MedyN <MedyN@mail.com>
2025-11-02 12:54:06 +0100

    Doing the asked job

--- script.py
+++ script.py
@@ -1,3 +1,7 @@
 print("Hello World")
 #good start, keep going with code
 #add a for loop that prints 10 elements
+
+for i in range(10):
+    print(i)   
+#completed the for loop to print numbers from 0 to 9
